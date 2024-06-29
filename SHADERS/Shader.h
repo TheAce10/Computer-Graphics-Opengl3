e0f52e494dda023af703e70f17ce601d5a564a73
Elikem <99412803+TheAce10@users.noreply.github.com>
2024-06-29 04:17:17 +0000

    Different Shaders in Graphics Programming

---
+++ SHADERS/Shader.h
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#pragma once
+
+class Shader
+{public:
+    // the program ID
+    unsigned int ID;
+    // constructor reads and builds the shader
+    Shader::Shader(const char* vertexPath, const char* fragmentPath);
+    // use/activate the shader
+    void use();
+
+    // utility uniform functions
+    void setBool(const std::string& name, bool value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+    }
+    void setInt(const std::string& name, int value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
+    void setFloat(const std::string& name, float value) const
+    {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
+};
+
